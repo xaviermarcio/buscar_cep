ad0887db62fa91cf20ae3b54b4dcdb152065727d
xaviermarcio80 <xaviermarcio80@gmail.com>
2023-08-28 14:02:36 -0300

    iniciando css do projeto

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const handleZipCode = (event) => {
+    let input = event.target
+    input.value = zipCodeMask(input.value)
+}
+  
+const zipCodeMask = (value) => {
+    if (!value) return ""
+    value = value.replace(/\D/g,'')
+    value = value.replace(/(\d{5})(\d)/,'$1-$2')
+    return value
+}
