80017590136cf80f237e829f86435094187dfe93
Márcio Xavier <43189549+xaviermarcio@users.noreply.github.com>
2023-08-28 17:16:10 -0300

    Finalizando Javascript

--- js/script.js
+++ js/script.js
@@ -9,3 +9,31 @@ const zipCodeMask = (value) => {
     value = value.replace(/(\d{5})(\d)/,'$1-$2')
     return value
 }
+function buscarCep(){
+    let input = document.getElementById('cep').value;
+
+    const ajax = new XMLHttpRequest();
+    ajax.open('Get' ,'https://viacep.com.br/ws/'+input+'/json/');
+    ajax.send();
+
+    ajax.onload = function(){
+        let obj = JSON.parse(this.responseText);
+        let logradouro = obj.logradouro;
+        let complemento = obj.complemento;
+        let bairro = obj.bairro;
+        let cidade = obj.localidade;
+        let estado = obj.uf;
+
+        document.getElementById('text').innerHTML = 
+        "<b>Logradouro:</b> " +logradouro + 
+        "<br><b>Complemento:</b> " +complemento+ 
+        "<br><b>Cidade:</b> " + cidade + 
+        "<br><b>Estado:</b> " + estado +
+        "<br><b>Bairro:</b> "+ bairro ;
+    }
+}
+
+function limparForm(){
+    document.getElementById('cep').value='';
+    document.getElementById('text').innerHTML ='';
+}
